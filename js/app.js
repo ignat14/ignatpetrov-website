f7326870ab1c39a3e080d1b9f7c2cd5147be322b
Ignat <ignat14@gmail.com>
2020-04-25 20:46:23 +0200

    stracture the main page

--- js/app.js
+++ js/app.js
@@ -2,8 +2,12 @@ const landing_page = document.getElementById("landing-page");
 const landing_page_bg = document.getElementById("landing-page-bg");
 const header_title = document.querySelector(".header-title");
 const transition_div = document.querySelector(".transition-div");
+const second_page = document.querySelector(".second-page");
+const about_text = document.querySelector(".about-text");
 
-// header_title.style.display = "none";
+second_page.style.display = "none";
+// landing_page.style.display = "none";
+// landing_page_bg.style.display = "none";
 
 const enter = function() {
 	// landing_page_bg.classList.add("open-site");
@@ -13,7 +17,11 @@ const enter = function() {
 	}, 300);
 	setTimeout(function() {
 		landing_page_bg.style.display = "none";
-		// header_title.style.display = "block";
+		second_page.style.display = "block";
 		header_title.classList.add("header-title-visible");
-	}, 1000);
+		about_text.classList.add("about-text-visible");
+	}, 500);
+
+	transformText(about_text, about_text.innerHTML, 18);
+
 }
--- js/title_anim.js
+++ js/title_anim.js
@@ -4,28 +4,34 @@ let innerText = text.innerHTML;
 const charSet = "10/{}&$%_#";
 let dynamicText = innerText;
 
-let cnt = 20
-const letterChangeInterval = setInterval(function() {
-	let newString = ""
-	for (let i = 0; i < innerText.length; i++) {
-		let charIdx = Math.floor(Math.random() * charSet.length)
-		let randomChar = charSet[charIdx];
-		let chance = Math.random() * innerText.length;
-		if (" .,".includes(innerText[i]) || chance > cnt) {
-			newString += innerText[i];
-		} else {
-			newString += randomChar;
+const transformText = function(element, inputText, cnt) {
+	
+	const letterChangeInterval = setInterval(function() {
+		let newString = ""
+		for (let i = 0; i < inputText.length; i++) {
+			
+			let charIdx = Math.floor(Math.random() * charSet.length)
+			let randomChar = charSet[charIdx];
+			let chance = Math.random() * inputText.length;
+			if (" .,".includes(inputText[i]) || chance > cnt) {
+				newString += inputText[i];
+			} else {
+				newString += randomChar;
+			}
 		}
-	}
-	
-	text.innerHTML = newString;
-	
-	if (cnt <= 1) {
-		clearInterval(letterChangeInterval);
-		text.innerHTML = innerText;
-	}
-	cnt--;
-}, 150);
+		console.log(newString);
+		
+		element.innerHTML = newString;
+		
+		if (cnt <= 1) {
+			clearInterval(letterChangeInterval);
+			element.innerHTML = inputText;
+		}
+		cnt--;
+	}, 150);
+}
+
+transformText(text, innerText, 20);
 
 
 
